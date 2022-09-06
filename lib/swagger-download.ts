fa8453f32a7aeb9f5da0d7e79353279bdc3c75c7
michael8512 <linshenghui321@sina.com>
2022-09-06 11:29:54 +0800

    WIP: update swagger-download

--- lib/swagger-download.ts
+++ lib/swagger-download.ts
@@ -2,31 +2,25 @@ import path from 'path'
 import { execSync } from 'child_process';
 import fs from "fs";
 import fetch from 'node-fetch';
-import { forEach } from 'lodash'
 import { logError, logSuccess, logInfo } from './util/log';
 import { ConfigProps } from './mock-server'
+import dotenv from 'dotenv';
 
-
-const startDowLoad = async (urlList: Array<{url: string}>) => {
-  const pList:Array<Promise<void>> = [];
-  forEach(urlList, ({url})=> {
-    let p:Promise<void> = new Promise((resolve)=> {
-      fetch(url, {
-        headers: {  }
-      }).then((res) => {
-        return res.json();
-      }).then(data => {
-        const title = data?.info?.title || Math.random().toFixed(0)
-        const _path = path.resolve(process.cwd(), `./config/swagger/${title}.json`);
-        logInfo(`download swagger:【${title}.json】`);
-        fs.writeFileSync(_path, JSON.stringify(data), 'utf8');
-        resolve();
-      });
+const startDowLoad = async (url: string) => {
+  new Promise<void>((resolve)=> {
+    fetch(url, {
+      headers: {  }
+    }).then((res) => {
+      return res.json();
+    }).then(data => {
+      const title = data?.info?.title || 'swagger'
+      const _path = path.resolve(process.cwd(), `./config/swagger/${title}.json`);
+      logInfo(`download swagger:【${title}.json】`);
+      fs.writeFileSync(_path, JSON.stringify(data), 'utf8');
+      resolve();
     });
-    pList.push(p);
   });
 
-  await Promise.all(pList);
   logSuccess('swagger files are update to date, bye 👋');
 }
 
@@ -36,19 +30,17 @@ const clearSwaggerFiles = () => {
 }
 
 const updateSwaggerFiles = async ()=> {
-  const configPath = path.join(process.cwd(), './config/config.json');
+  const configPath = path.resolve(process.cwd(), `./easy-service-config/.env`);
 
   if (!fs.existsSync(configPath)) {
     logError('please execute "npm run setup" first!')
     process.exit(1);
   }
 
-  const { linkFiles }: ConfigProps = await require(configPath);
-
-  console.log(linkFiles)
+  const config: ConfigProps = dotenv.config({ path: configPath }).parsed as any;
 
   clearSwaggerFiles();
-  startDowLoad(linkFiles);
+  startDowLoad(config.API_URL);
 }
 
 updateSwaggerFiles();
